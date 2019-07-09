c5a035350af664544f2c1f1eedf01323171ae0aa
a-8-a <cogksmf02@naver.com>
2019-07-09 15:10:39 +0900

    Add get image

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const expressSession = require('express-session');
+const fs = require('fs');
 
 const adminPage = require('./routes/admin.js');
 const itempPage = require('./routes/item.js');
@@ -24,6 +25,14 @@ app.use('/uploads', express.static('uploads'));
 
 app.use('/admin', adminPage);
 app.use('/item', itempPage);
+
+app.get('/uploads/:id', function(req, res){
+  if (!fs.existsSync(__dirname+'/uploads/'+req.params.id)){
+    res.status(404).json({message:'Not found'});
+    return;
+  }
+  res.status(200).sendfile(__dirname+'/uploads/'+req.params.id);
+});
 
 
 app.listen(3000, function(){
